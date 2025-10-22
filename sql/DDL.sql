26370c28fc378735a633f433091ec596b9b8d728
ShinByungTak <shinbat@daum.net>
2025-10-23 05:50:04 +0900

    DDL, crud 생성

---
+++ sql/DDL.sql
@@ -0,0 +1,41 @@
+/*
+ * DB 는 localDB 사용
+ * 
+ * 작성자: 신병탁
+ * 날짜: 2025-10-23
+ */
+
+CREATE DATABASE 1st_pjt;
+USE  1st_pjt;
+
+DROP TABLE IF EXISTS  drivers;
+DROP TABLE IF EXISTS country;
+CREATE TABLE country (
+	country_id INT PRIMARY key AUTO_INCREMENT COMMENT '나라 아이디',
+  	country_name VARCHAR(50) NOT NULL UNIQUE COMMENT '나라이름',
+  	description VARCHAR(500) COMMENT '설명',
+  	create_date DATE NOT NULL COMMENT '생성일시'
+) COMMENT='나라 정보';
+
+DROP TABLE IF EXISTS team;
+CREATE TABLE team (
+	team_id INT PRIMARY key AUTO_INCREMENT COMMENT '팀 아이디',
+  	teamy_name VARCHAR(50) NOT NULL UNIQUE COMMENT '팀이름',
+  	description VARCHAR(500) COMMENT '설명',
+  	create_date DATE NOT NULL COMMENT '생성일시'
+) COMMENT='팀 정보';
+
+DROP TABLE IF EXISTS  drivers;
+CREATE table drivers (
+	driver_id INT PRIMARY key AUTO_INCREMENT COMMENT '아이디',
+  	name VARCHAR(50) NOT NULL COMMENT '이름',
+  	team_id INT COMMENT '팀 아이디',
+  	country_id INT   NOT NULL COMMENT '나라 아이디',
+  	email varchar(100) unique key COMMENT '이메일',
+  	gender char(1) check(gender in('m', 'f')) COMMENT '성별(m,f)',
+  	description VARCHAR(500) COMMENT '설명',
+  	image_url text NULL DEFAULT NULL COMMENT '이미지',
+  	create_date DATE NOT NULL COMMENT '생성일시',
+	CONSTRAINT fk_team FOREIGN KEY(team_id) REFERENCES team(team_id),
+    CONSTRAINT fk_country FOREIGN KEY(country_id) REFERENCES country(country_id)
+) COMMENT='드라이버 정보';
--- sql/drivers.py
+++ sql/drivers.py
@@ -2,3 +2,35 @@
 작성자: 김가람
 날짜: 2025-10-22
 '''
+
+# drivers_db.py
+def insert_team(name, email, tall, birthday):
+    pass
+def insert_country(name, email, tall, birthday):
+    pass
+def insert_driver(name, team, country, img):
+    pass
+
+def update_team(team_id):
+    pass
+def update_country(country_id):
+    pass
+def update_driver(driver_id):
+    pass
+
+def delee_team(team_id):
+    pass
+def delete_country(country_id):
+    pass
+def delete_driver(driver_id):
+    pass
+
+
+def select_team():
+    pass # 전체조회
+def select_country():
+    pass # 전체조회
+def select_driver():
+    pass # 전체조회
+def select_driver_by_id(driver_id):
+    pass # ID로 조회
