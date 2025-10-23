b9f97ba2c60c7137ccbb98fb64ccda73d3bc2dca
Kevin Hyunwoo Sohn <kevinhwsohn@gmail.com>
2025-10-24 08:49:32 +0900

    화면제구성및 수정

--- app.py
+++ app.py
@@ -29,9 +29,9 @@ st.markdown("""
    <style>
 """, unsafe_allow_html=True)
 
-st.markdown('<h1 class="fade-title">FAST F1 페이지</h1>', unsafe_allow_html=True)
+st.markdown('<h1 class="fade-title">FAST F1 DATABASE</h1>', unsafe_allow_html=True)
 
-st.write("빨리빨리 F1 웹사이트에 오신것을 환영합니다. 원하시는 메뉴를 선택해 주세요")
+st.write("Welcome to Fast F1 Website. Korea's very own fast service F1 Database. Please Select Your Menu")
 
 st.image("https://www.grandprix247.com/wp-content/uploads/2025/04/f1-movie-brad-pitt-poster-750x400.jpg", caption="F1 Movie Poster Copyright © Apple Films 2025", use_column_width=True)
 
@@ -40,9 +40,11 @@ col1, col2, col3 = st.columns(3)
 if col1.button('Main Menu'):
    st.switch_page("app.py") 
 
-if col2.button('F1 운전자들'):
+if col2.button('F1 Drivers'):
    st.switch_page("driver_intro.py")
 
 if col3.button('F1 Driver Comparison'):
    st.switch_page("driver_comparison(test).py")
-
+   
+st.title("About Us")
+st.write("Much like our beloved F1 drivers, Fast F1 Database is a page that is dedicated to bring the fastest and latest update on F1. This includes race updates, scores, player analysis, track analysis and plenty of other F1 related medias and news. Our Database will always strive to provide the most reliable and fastest database related with F1, and we hope to share our love and interest with all the fans of the greatest racing event in the world.")
