3a820b0f5b68cdcba672113554f1b2f5c582a3ea
lucy <lucymoon.d@gmail.com>
2025-10-24 10:48:55 +0900

    driver_comparison update

--- pages/driver_comparison.py
+++ pages/driver_comparison.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from sql.drivers import DriversDBManager
+from data.driver import DriverData
 
 st.set_page_config(page_title="F1 Drivers", page_icon="data/logo.png")
 st.logo("data/logo.png")
@@ -8,22 +9,6 @@ st.logo("data/logo.png")
 col1, col2 = st.columns([1, 1])
 
 driversDB = DriversDBManager()
-driver_list = driversDB.select_drivers()
+driver_list: DriverData = driversDB.select_drivers()
 
 st.text(driver_list)
-
-# with col1:
-#     d = st.selectbox(
-#         label = "레이서 선택",
-#         options = driver_list
-#     )
-
-#     st.text(d)
-
-# with col2:
-#     d = st.selectbox(
-#         label = "레이서 선택",
-#         options = driver_list
-#     )
-
-#     st.text(d)
